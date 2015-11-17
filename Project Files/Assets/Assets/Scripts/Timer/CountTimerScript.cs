25f6c30d5f17880da3a11b9f4826bb94dde7aea5
KingdomHearts <michaelbos@live.nl>
2015-11-17 17:56:21 +0100

    Last Commit of 17-10. Nodes are created.
    Nodes and grid are created with code. Also an update to time script and
    Method IsTimeDown is not working yet.

--- Project Files/Assets/Assets/Scripts/Timer/CountTimerScript.cs
+++ Project Files/Assets/Assets/Scripts/Timer/CountTimerScript.cs
@@ -4,8 +4,9 @@ using System.Collections;
 /*
 Made by: Michael Bossink
 Used for: Unity Countdown timers
-Version: 1.1
+Version: 1.3
 Date: 17-11-2015
+Last Change: Typo. And <para></para>
 */
 
 public static class CountTimerScript {
@@ -34,11 +35,11 @@ public static class CountTimerScript {
         return Time.time + (pMinutes * 60) + pSeconds;
     }
     /// <summary>
-    /// <para>An easy to use timer.<para />
-    /// <para>pStartTime = Time.time<para />
-    /// <para>pEndTime is default Fill in 0.<para />
-    /// <para>pMinutes is AddMinutes.<para />
-    /// <para>pSeonds is AddSeconds.<para />
+    /// <para>An easy to use timer. Work in progress</para>
+    /// <para>pStartTime = Time.time/<para>
+    /// <para>pEndTime is default Fill in 0.</para>
+    /// <para>pMinutes is AddMinutes.</para>
+    /// <para>pSeconds is AddSeconds./<para>
     /// </summary>
     public static bool IsTimerDown(float pStartTime = 0,float pEndTime = 0, float pMinutes = 0, float pSeconds = 0)
     {
