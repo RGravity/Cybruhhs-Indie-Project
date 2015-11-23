e8cd6b2a630ff202cf938247d55ae3904e01e162
wargamer88 <m.van.denzel@home.nl>
2015-11-23 12:08:40 +0100

    Added Tiled
    added tiled: test levels and Tiles

---
+++ Project Files/Assets/Assets/Tiled/Tiles/Tileset.tsx
@@ -0,0 +1,18 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset name="Tileset" tilewidth="64" tileheight="64" tilecount="5">
+ <tile id="0">
+  <image width="64" height="64" source="Base.png"/>
+ </tile>
+ <tile id="1">
+  <image width="64" height="64" source="InsideCamera.png"/>
+ </tile>
+ <tile id="2">
+  <image width="64" height="64" source="OutsideCamera.png"/>
+ </tile>
+ <tile id="3">
+  <image width="64" height="64" source="Road.png"/>
+ </tile>
+ <tile id="4">
+  <image width="64" height="64" source="Spawn.png"/>
+ </tile>
+</tileset>
