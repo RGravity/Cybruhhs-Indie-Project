69e5e054776692155cf4602bf9e47ca5c0fdd419
RGravity <rogieroh@hotmail.com>
2015-11-26 10:52:07 +0100

    Building and Pauze
    Done and DOne

---
+++ Project Files/Assets/Assets/Scripts/MenuScripts/PauseScript.cs
@@ -0,0 +1,67 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PauseScript : MonoBehaviour {
+
+    private bool _pauseGame = false;
+    private GameObject _resumeButton;
+    private GameObject _quitButton;
+    private GameObject _backGround;
+    private GameObject _pauseButton;
+    private GameObject _radialMenu;
+
+    public bool PauseGame { get { return _pauseGame; } set { _pauseGame = value; } }
+
+	// Use this for initialization
+	void Start () {
+        _resumeButton = GameObject.Find("ResumeButton");
+        _quitButton = GameObject.Find("QuitButton");
+        _backGround = GameObject.Find("OverlayPause");
+        _pauseButton = GameObject.Find("PauseButton");
+        _radialMenu = GameObject.FindObjectOfType<BuildingSpawnScript>().gameObject;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        _checkInput();
+        _checkPause();
+	}
+
+    private void _checkInput()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            _pauseGame = !_pauseGame;
+        }
+    }
+
+    /// <summary>
+    /// Pauses the game if Key is pressed or button is clicked
+    /// </summary>
+    private void _checkPause()
+    {
+        if (_pauseGame)
+	    {
+            Time.timeScale = 0;
+            _resumeButton.SetActive(true);
+            _quitButton.SetActive(true);
+            _backGround.SetActive(true);
+            _pauseButton.SetActive(false);
+            _radialMenu.SetActive(false);
+	    }
+        else
+        {
+            Time.timeScale = 1;
+            _resumeButton.SetActive(false);
+            _quitButton.SetActive(false);
+            _backGround.SetActive(false);
+            _pauseButton.SetActive(true);
+            _radialMenu.SetActive(true);
+        }
+    }
+
+    public void QuitGame()
+    {
+        Application.LoadLevel(0);
+    }
+}
