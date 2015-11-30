0495ed37e36d604a25331f1a20cb11bd8c40d115
wargamer88 <m.van.denzel@home.nl>
2015-11-30 15:35:40 +0100

    Wave code

---
+++ Project Files/Assets/Assets/Scripts/HUD/WaveIndicatorScript.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class WaveIndicatorScript : MonoBehaviour {
+
+    private string _waveText;
+
+    void start()
+    {
+        _waveText = GetComponent<Text>().text;
+    }
+
+	public void SetWaveIndicator(int pCurrentWave, int pTotalWaves)
+    {
+        GetComponent<Text>().text = pCurrentWave + "/" + pTotalWaves;
+    }
+}
