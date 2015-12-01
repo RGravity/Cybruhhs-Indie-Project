e029ec5d1657e11fce5d9353d47bbd0b8009c75e
wargamer88 <m.van.denzel@home.nl>
2015-12-01 12:01:33 +0100

    Added Tower Placement tiles

---
+++ Project Files/Assets/Assets/Scripts/Buildings/BuildPlacementTilesScript.cs
@@ -0,0 +1,10 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class BuildPlacementTilesScript : MonoBehaviour {
+
+    [SerializeField]
+    private TowerNoneNumbers _towerNumber;
+
+    public TowerNoneNumbers TowerPlaceNr { get { return _towerNumber; }}
+}
