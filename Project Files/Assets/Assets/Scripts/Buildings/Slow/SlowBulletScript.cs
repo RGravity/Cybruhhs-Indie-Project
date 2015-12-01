7674abac9700315078152d16a88bf61eb0a3821d
KingdomHearts <michaelbos@live.nl>
2015-12-01 12:15:46 +0100

    Rename SlowBulletScript. And made prefabs of Cannon and Slow

--- Project Files/Assets/Assets/Scripts/Buildings/Slow/SlowBulletScript.cs
+++ Project Files/Assets/Assets/Scripts/Buildings/Slow/SlowBulletScript.cs
@@ -1,7 +1,7 @@
 ﻿using UnityEngine;
 using System.Collections;
 
-public class SlowTowerArrow : MonoBehaviour {
+public class SlowBulletScript : MonoBehaviour {
 
 	// Use this for initialization
 	void Start () {
