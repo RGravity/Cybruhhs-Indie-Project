379b64e241a085f215fa0c2643ccbc734ed44d45
Elias1355 <eliasdragon55@hotmail.com>
2015-12-02 11:02:51 +0100

    Commit sound&music update + UpgradeTowerPinwheel
    This commit has the sound & music update and the upgradetowerpinwheel,
    still need the images. The upgrade tower pinwheel does not work with the
    lost of money, YET!

---
+++ Project Files/Assets/Assets/Scripts/UpgradeScripts/CancelButtonScript.cs
@@ -0,0 +1,41 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CancelButtonScript : MonoBehaviour {
+
+    // Use this for initialization
+    private UpgradeTowerScript [] _upgrade;
+    private bool _check = false;
+
+
+    void Start ()
+    {
+        _upgrade = GameObject.FindObjectsOfType<UpgradeTowerScript>();
+	}
+	
+	// Update is called once per frame
+	void Update ()
+    {
+        check ();
+    }
+
+
+    private void check()
+    {
+        if (_check == true)
+        {
+            _upgrade = GameObject.FindObjectsOfType<UpgradeTowerScript>();
+            _check = false;
+        }
+    }
+
+    public void ClickOn()
+    {
+        _check = true;
+        for (int i = 0; i < _upgrade.Length; i++)
+        {
+            _upgrade[i].Disappear = true;
+        }
+       
+    }
+}
