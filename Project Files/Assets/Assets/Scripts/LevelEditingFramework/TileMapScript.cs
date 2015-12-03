ce1b3ee851eacf57e5dad85e5444b8a0c9c85e16
wargamer88 <m.van.denzel@home.nl>
2015-12-03 15:18:25 +0100

    after GE Grade

--- Project Files/Assets/Assets/Scripts/LevelEditingFramework/TileMapScript.cs
+++ Project Files/Assets/Assets/Scripts/LevelEditingFramework/TileMapScript.cs
@@ -273,70 +273,7 @@ public class TileMapScript : MonoBehaviour
     {
         // Initialize the array
         _graph = GenerateNodeMap.GeneratePathfindingGraph(_mapSizeX, _mapSizeY);
-        #region MyRegion
-        /*_graph = new NodeScript[_mapSizeX, _mapSizeY];
-
-        // Initialize a Node for each spot in the array
-        for (int x = 0; x < _mapSizeX; x++)
-        {
-            for (int y = 0; y < _mapSizeY; y++)
-            {
-                _graph[x, y] = new NodeScript();
-                _graph[x, y].X = x;
-                _graph[x, y].Y = y;
-                #region create the node
-                //GameObject node = GameObject.CreatePrimitive(PrimitiveType.Sphere);
-                //node.transform.position = new Vector3(x, y, -1);
-                //node.transform.localScale = new Vector3(node.transform.localScale.x/2, node.transform.localScale.y/2, node.transform.localScale.z/2);
-                #endregion
-            }
-        }
-
-        // Now that all the nodes exist, calculate their neighbours
-        for (int x = 0; x < _mapSizeX; x++)
-        {
-            for (int y = 0; y < _mapSizeY; y++)
-            {
-
-                // This is the 4-way connection version:
-                if (x > 0)
-                    _graph[x, y].Neighbours.Add(_graph[x - 1, y]);
-                if (x < _mapSizeX - 1)
-                    _graph[x, y].Neighbours.Add(_graph[x + 1, y]);
-                if (y > 0)
-                    _graph[x, y].Neighbours.Add(_graph[x, y - 1]);
-                if (y < _mapSizeY - 1)
-                    _graph[x, y].Neighbours.Add(_graph[x, y + 1]);
-
-                #region diagonal
-                // This is the 8-way connection version (allows diagonal movement)
-                //// Try left
-                //if(x > 0) {
-                //	graph[x,y].neighbours.Add( graph[x-1, y] );
-                //	if(y > 0)
-                //		graph[x,y].neighbours.Add( graph[x-1, y-1] );
-                //	if(y < mapSizeY-1)
-                //		graph[x,y].neighbours.Add( graph[x-1, y+1] );
-                //}
-
-                //// Try Right
-                //if(x < mapSizeX-1) {
-                //	graph[x,y].neighbours.Add( graph[x+1, y] );
-                //	if(y > 0)
-                //		graph[x,y].neighbours.Add( graph[x+1, y-1] );
-                //	if(y < mapSizeY-1)
-                //		graph[x,y].neighbours.Add( graph[x+1, y+1] );
-                //}
-
-                //// Try straight up and down
-                //if(y > 0)
-                //	graph[x,y].neighbours.Add( graph[x, y-1] );
-                //if(y < mapSizeY-1)
-                //	graph[x,y].neighbours.Add( graph[x, y+1] );
-                #endregion
-            }
-        } */
-        #endregion
+        
     }
     /// <summary>
     /// <para>Create the tiles based on tiles that you make in the _generateMapData</para>
