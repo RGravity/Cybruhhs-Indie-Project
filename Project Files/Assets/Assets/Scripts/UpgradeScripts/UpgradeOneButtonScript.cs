a5bbe806960bd95a617865eb6554ecaa047dfa36
Elias1355 <eliasdragon55@hotmail.com>
2015-12-04 07:05:53 +0100

    Commit "Finished upgrade towers and whatever"

--- Project Files/Assets/Assets/Scripts/UpgradeScripts/UpgradeOneButtonScript.cs
+++ Project Files/Assets/Assets/Scripts/UpgradeScripts/UpgradeOneButtonScript.cs
@@ -3,20 +3,14 @@ using System.Collections;
 using UnityEngine.UI;
 
 public class UpgradeOneButtonScript : MonoBehaviour {
-    
-    private bool _spider = false;
-    private bool _tree = false;
-    private bool _troll = false;
 
-    public bool Spider { get { return _spider; } set { _spider = value; } }
-    public bool Tree { get { return _tree; } set { _tree = value; } }
-    public bool Troll { get { return _troll; } set { _troll = value; } }
-
-	// Use this for initialization
-	void Start ()
+    private int _intUpdate;
+    public int IntUpdate { get { return _intUpdate; } set { _intUpdate = value; } }
+    // Use this for initialization
+    void Start ()
     {
-       
-	}
+        
+    }
 	
 	// Update is called once per frame
 	void Update ()
@@ -24,20 +18,33 @@ public class UpgradeOneButtonScript : MonoBehaviour {
 	
 	}
 
-
     public void OnClickSpider()
     {
-        if (gameObject.GetComponent<Image>().sprite == Resources.Load<Sprite>("SpiderUpgrade1"))_spider = true;
+        if (gameObject.GetComponent<Image>().sprite == Resources.Load<Sprite>("SpiderUpgrade1"))
+        {
+          //  _upgrade.TempSlow.UpdateTowerSlow();
+           // _upgrade.Disappear = true;
+        }
     }
 
     public void OnClickTree()
     {
-        if (gameObject.GetComponent<Image>().sprite == Resources.Load<Sprite>("TreeUpgrade1"))_tree = true;
+        if (gameObject.GetComponent<Image>().sprite == Resources.Load<Sprite>("TreeUpgrade1"))
+        {
+          //  _upgrade.TempArrow.UpdateTowerArrow();
+          //  _upgrade.Disappear = true;
+        }
     }
 
     public void OnClickTroll()
     {
-        if (gameObject.GetComponent<Image>().sprite == Resources.Load<Sprite>("TrollUpgrade1"))_troll = true;
+       
+        if (gameObject.GetComponent<Image>().sprite == Resources.Load<Sprite>("TrollUpgrade1"))
+        {
+            IntUpdate = 1;
+            //_cannon = pTower;
+            //_cannon.UpdateTowerCannon();
+        }
     }
 
 }
